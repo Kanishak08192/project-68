f797d18c9a8417dfb8d3027d55afa500acb04c20
Kanishak08192 <kanishak2007@gmail.com>
2021-03-09 15:50:21 +0530

    commit1

---
+++ screens/Instagram.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Instagram extends React.Component{
+    render(){
+        return(
+            <View>
+            <Text>Instagram</Text>
+
+            </View>
+        )
+    }
+}
